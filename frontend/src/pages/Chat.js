2e35d084f496dc5d186e2cd4b68af822f68c8d38
emmaolofsson1 <olofsson2500@gmail.com>
2025-07-31 17:40:52 -0500

    Create Chat.js

---
+++ frontend/src/pages/Chat.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ChatWindow from "../components/ChatWindow";
+
+const ChatPage = () => (
+  <div className="chat-page">
+    <h2>Ask EduAI</h2>
+    <ChatWindow />
+  </div>
+);
+
+export default ChatPage;
